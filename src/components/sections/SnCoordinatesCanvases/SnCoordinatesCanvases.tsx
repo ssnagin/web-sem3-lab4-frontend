a009197d894437b5faf85b305e41aab91615b56f
ssnagin <56646530+ssnagin@users.noreply.github.com>
2025-12-26 18:22:04 +0300

    sync canvas with dots (without websockets)

--- src/components/sections/SnCoordinatesCanvases/SnCoordinatesCanvases.tsx
+++ src/components/sections/SnCoordinatesCanvases/SnCoordinatesCanvases.tsx
@@ -1,27 +1,51 @@
-import { SnCanvas } from '../../ui/canvas/SnCanvas';
+import {SnCanvas, type SnPoint} from '../../ui/canvas/SnCanvas';
 import {useSelector} from "react-redux";
 import type {RootState} from "../../../redux/store.ts";
 import classes from "./SnCoordinatesCanvases.module.scss";
 import {Dropdown} from "primereact/dropdown";
+import {useEffect, useState} from "react";
 
-function onClick() {
+const fetchPoints = async (token: string): Promise<SnPoint[]> => {
+    const res = await fetch('https://itmo.ssngn.ru/lab4/api/point/all', {
+        method: "GET",
+        headers: {
+            'snAuthToken': token,
+        },
+    });
+    if (!res.ok) throw new Error('Failed to fetch points');
 
-}
+    const json = await res.json();
+    console.log("ALL DATA", json)
+
+    return json.data.map((p: any) => ({
+        x: p.x,
+        y: p.y,
+        hit: p.inArea,
+    }));
+};
 
 export const SnCoordinatesCanvases = () => {
 
-    const currentRadius = useSelector((state: RootState) => state.form.r)
+    const { token } = useSelector((state: RootState) => state.auth);
+    const currentRadius = useSelector((state: RootState) => state.form.r);
+    const [points, setPoints] = useState<SnPoint[]>([]);
 
-    const testPoints = [
-        { x: 1, y: 1, hit: true },
-        { x: -1, y: -1, hit: false }
-    ];
+    useEffect(() => {
+        if (!token) return;
+        fetchPoints(token)
+            .then(setPoints)
+            .catch(console.error);
+    }, [token]);
+
+    const handlePointClick = () => {
+        // оставлю пустым пока
+    };
 
     return (
         <>
             <div className={classes.canvases + " row"}>
                 <div className={"col-lg-9"}>
-                    <SnCanvas  r={Number(currentRadius)} points={testPoints} onPointClick={onClick}/>
+                    <SnCanvas  r={Number(currentRadius)} points={points} onPointClick={handlePointClick}/>
                 </div>
                 <div className={"col-lg-3"}>
                     <h3><b>Скины:</b></h3>
